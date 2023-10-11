c7b8de36a556fe0d7a0c2c2f462d2928a8e851c3
unknown <devendra13.dc@gmail.com>
2023-10-11 14:56:35 +0545

    ...

--- components/Sections/GallerySection.tsx
+++ components/Sections/GallerySection.tsx
@@ -5,7 +5,7 @@ import GalleryCard from '../Cards/GalleryCard'
 
 const GallerySection = () => {
   return (
-    <div className='gallerySection w-[90%] py-[50px] flex flex-col items-center '>
+    <div className='gallerySection w-full md:w-[90%] px-[20px] py-[50px] flex flex-col items-center '>
          <div className='w-full flex items-center justify-center gap-[50px]'>
             <h1 className='font-poppins text-xl font-bold tracking-[2px] underline underline-offset-8 decoration-primaryBlue'>
                 Our Gallery
