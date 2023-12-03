8347f982d619202b30b31827b514d95b87069c9a
unknown <devendra13.dc@gmail.com>
2023-12-03 07:53:11 +0545

    working on testimonial card

--- components/Cards/TestimonialCard.tsx
+++ components/Cards/TestimonialCard.tsx
@@ -1,29 +1,41 @@
 import Image from "next/image";
-import { comma } from "postcss/lib/list";
 import React from "react";
 
 const TestimonialCard = ({ data }: any) => {
   const { name, image, designation, summary } = data;
+
   return (
     <div className="w-full bg-slate-200 rounded-[4px] p-[20px] shadow-lg shadow-gray-600 overflow-hidden">
-      <div className="flex items-center gap-[20px]">
-        <div className="relative min-w-[60px] min-h-[60px] max-w-[60px] max-h-[60px]">
-          <Image
-            src={image}
-            alt="personImage"
-            fill
-            className="object-cover rounded-full "
-          />
-        </div>
+      <div
+        className="relative overflow-hidden"
+        style={{
+          width: '300px', // Adjust the desired width
+          height: '180px', // Adjust the desired height
+        }}
+      >
+        <Image
+          src={image}
+          alt="personImage"
+          layout="fill"
+          objectFit="cover"
+          className=""
+          style={{
+            WebkitMaskImage: 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 20 12\'%3E%3Cpath d=\'M 0 0 H 12 C 10 6 4 4 0 8 V 0\' fill=\'white\'/%3E%3C/svg%3E")',
+            maskImage: 'url("data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 20 12\'%3E%3Cpath d=\'M 0 0 H 12 C 10 6 4 4 0 8 V 0\' fill=\'white\'/%3E%3C/svg%3E")',
+            maskSize: '100%',
+            WebkitMaskRepeat: 'no-repeat',
+            maskRepeat: 'no-repeat',
+          }}
+        />
+      </div>
 
-        <div>
-          <p>{name}</p>
-          <p className="font-brunoAce text-lg font-bold">{designation}</p>
-        </div>
+      <div>
+        <p>{name}</p>
+        <p className="font-brunoAce text-lg font-bold">{designation}</p>
       </div>
 
       <div className="px-[70px] mt-[20px]">
-        <div dangerouslySetInnerHTML={{__html: summary}} className="text-xl" />
+        <div dangerouslySetInnerHTML={{ __html: summary }} className="text-xl" />
       </div>
     </div>
   );
---
+++ components/SvgShape/SvgShape1.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SvgShape1 = () => {
+    return (
+        <svg viewBox="0 0 10 6" width="100">
+            <path
+                d="M 0 0 H 6 C 5 3 2 2 0 4 V 0"
+                stroke="#000"
+                strokeWidth="0.01"
+                fill="orange"
+            />
+        </svg>
+    )
+}
+
+export default SvgShape1
