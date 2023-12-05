7ee28a8364b12c8378c8385c8192a9c3364d5891
pratimabudhathoki <devendra13.dc@gmail.com>
2023-12-05 16:14:02 +0545

    ...

--- components/Sections/VideoSection.tsx
+++ components/Sections/VideoSection.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 
 const VideoSection = ({ data }: any) => {
-
   return (
     <div className="w-full lg:w-[90%] py-[30px] lg:py-[70px]">
       <p className="font-brunoAce underline underline-offset-8 decoration-primaryBlue text-center font-bold text-xl tracking-[2px]">
@@ -12,9 +11,8 @@ const VideoSection = ({ data }: any) => {
       <div className="w-full mt-[50px] font-montserrat">
         {data?.length > 0 &&
           data.map((item: any, index: number) => (
-            <div className="w-full lg:w-[30%] p-[15px]">
+            <div key={index} className="w-full lg:w-[30%] p-[15px]">
               <iframe
-                key={index}
                 width="100%"
                 height="275"
                 src="https://www.youtube.com/embed/DGm_rLTrduI"
